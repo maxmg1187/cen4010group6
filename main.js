33656e6c77cc2801119b934dee7ea4f5e5bd419e
shiitake2200 <141689097+shiitake2200@users.noreply.github.com>
2023-10-29 14:51:22 -0400

    Recent Update

--- main.js
+++ main.js
@@ -4,14 +4,18 @@ const openai = new OpenAI({
   apiKey: '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',
 });
 const prompt = 'Translate the following English text to French: "Hello, how are you?"';
+const inputElement = document.getElementById("chatgptinput");
+const outputElement = document.getElementById("chatgptoutput");
 
-async function main() {
+async function sendRequest() {
   const chatCompletion = await openai.chat.completions.create({
     messages: [{ role: 'user', content: prompt }],
     model: 'gpt-3.5-turbo',
   });
-
-  console.log(chatCompletion.choices[0].message.content);
 }
 
-main();
+inputElement.addEventListener("input", function(){
+  prompt = inputElement.textContent();
+  sendRequest();
+  outputElement.textContent = chatCompletion.choices[0].message.content;
+})
