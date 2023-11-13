776108b482bbcd61100388cbd75765f11f6f529a
Thuong Nguyen <tnguy147@fiu.edu>
2023-11-13 13:25:14 -0500

    none

--- playbutton.js
+++ playbutton.js
@@ -1,26 +1,56 @@
 // Get references to the button and container
-const button = document.getElementById('playButton');
+const playButton = document.getElementById('playButton');
+const muteButton = document.getElementById('muteButton');
 const container = document.getElementById('header');
+const audio = new Audio();
+const chatBox = document.querySelector('.chatGPT');
 
- // Array of image URLs
- const images = [
-    'images/spring.jpg',
-    'images/summer.jpg',
-    'images/autumn.jpg',
-    'images/winter.jpg'
+ // Array of image and song URLs
+ const imageAndSong = [
+    { image: 'images/spring.jpg', audio: 'audio/spring.mp3' },
+    { image: 'images/summer.jpg', audio: 'audio/summer.mp3' },
+    { image: 'images/autumn.jpg', audio: 'audio/autumn.mp3' },
+    { image: 'images/winter.jpg', audio: 'audio/winter.mp3' }
 ];
 
-// Function to change the background image
-function changeBackground() {
-// Select a random index from the images array
-const randomIndex = Math.floor(Math.random() * images.length);
-
-// Get the random image URL
-const randomImage = images[randomIndex];
-
-// Set the background image
-container.style.backgroundImage = `url(${randomImage})`;
-}
 
 // Add a click event listener to the button
-button.addEventListener('click', changeBackground);
+playButton.addEventListener('click', changeBackgroundAndAudio);
+
+// Function to change the background image and song
+function changeBackgroundAndAudio(){
+// Select a random index
+const randomIndex = Math.floor(Math.random() * imageAndSong.length);
+
+// Get the random image URL
+const random = imageAndSong[randomIndex];
+
+// Set the background image
+container.style.backgroundImage = `url(${random.image})`;
+
+// Set the audio source and play the audio
+audio.src = random.audio;
+audio.loop = true;
+audio.play();
+
+muteButton.style.display = 'flex';
+
+
+playButton.style.display = 'none';
+
+chatBox.style.display = 'flex';
+}
+
+muteButton.addEventListener('click', toggleMute);
+
+function toggleMute() {
+    if (audio.muted) {
+        audio.muted = false;
+        muteButton.classList.remove('fa-volume-mute');
+        muteButton.classList.add('fa-volume-up');
+      } else {
+        audio.muted = true;
+        muteButton.classList.remove('fa-volume-up');
+        muteButton.classList.add('fa-volume-mute');
+    }
+}
